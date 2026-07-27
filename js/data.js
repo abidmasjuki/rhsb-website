4172022e9293cd94441e556169eb24d7d98d504e
RH-Abid <customer@rpwphealthcare.com>
2026-07-27 08:57:42 +0800

    feat: apply photography rules for face centering and generate clinical audiologist portrait for Natasha

--- js/data.js
+++ js/data.js
@@ -81,7 +81,7 @@ const RPWP_DATA = {
       role: "Lead Clinical Audiologist",
       qualifications: "Bachelor of Audiology (Hons) (USM)",
       bio: "Leads clinical hearing assessments, hearing aid prescription tuning, and custom earmold fitting at INNOHEAR Kajang.",
-      imgUrl: "images/advisories/natasha.png"
+      imgUrl: "images/advisories/natasha.jpg"
     },
     {
       id: "abid",
